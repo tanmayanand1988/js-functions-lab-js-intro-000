09dabd102f0f65af720d63162db3d83b724a30d4
Tanmay Anand <tanmayanand@gmail.com>
2020-06-16 03:11:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function happyHolidays(){
+    return "Happy holidays!"
+}
+function happyHolidaysto(string){
+    return `Happy holidays, ${string}`
+}
+function happyCustomHolidayTo(holiday, name){
+    return `Happy ${holiday}, ${name}!`
+}
+
 // write your code below!
